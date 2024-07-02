28ea78f002da9cd48b9ffe78eac8397a950b18a0
kilian <audrb96@gmail.com>
2024-07-02 13:44:29 +0900

    refactor : yml-importer 모듈 위치 변경

--- core/core-domain/roomescape-reservation-domain/src/main/java/roomescape/error/code/DomainErrorCode.java
+++ core/core-domain/roomescape-reservation-domain/src/main/java/roomescape/error/code/DomainErrorCode.java
@@ -6,7 +6,7 @@ public enum DomainErrorCode {
     THIS_VALUE_TO_BE_NULL("이 값은 NULL 이어서는 안됩니다."),
     CANNOT_CREATE_RESERVATION_FOR_PAST_TIME("지나간 시간에는 예약을 할 수 없습니다."),
     NOT_FOUND_RESERVATION("예약을 찾을 수 없습니다."),
-    NOT_FOUND_RESERVATION_TIME("예약 시간을 찾을 수 없습니다."),
+    NOT_FOUND_RESERVATION_TIME("예약 시간 찾을 수 없습니다."),
     NOT_FOUND_THEME("테마를 찾을 수 없습니다."),
     NOT_FOUND_USER("사용자를 찾을 수 없습니다.");
 
